429d9a09b07a11fec07479cb87209a7aac542783
unknown <isahin16@ku.edu.tr>
2020-12-03 00:14:52 +0300

    opened classes and wrote methods

--- src/Domain/GameObjects/Molecule.java
+++ src/Domain/GameObjects/Molecule.java
@@ -1,5 +1,43 @@
 package Domain.GameObjects;
+import java.awt.Point;
 
 public class Molecule {
-
+	public int moleculeID;
+	private Point coordinate;
+	private boolean isSpinning;
+	private boolean isLinear;
+	public Molecule (int moleculeID, Point coordinate) {
+		this.moleculeID = moleculeID;
+		this.coordinate = coordinate;
+		this.isSpinning = false;
+		this.isLinear = false;
+	}
+	public Molecule (int moleculeID, Point coordinate,boolean isSpinning, boolean isLinear) {
+		this.moleculeID = moleculeID;
+		this.coordinate = coordinate;
+		this.isSpinning = isSpinning;
+		this.isLinear = isLinear;
+	}
+	public void setCoordinate(Point coordinate) {
+		this.coordinate = coordinate;
+	}
+	public Point getCoordinate() {
+		return this.coordinate;
+	}
+	public boolean isSpinning() {
+		return isSpinning;
+	}
+	public void setSpinning(boolean isSpinning) {
+		this.isSpinning = isSpinning;
+	}
+	public boolean isLinear() {
+		return isLinear;
+	}
+	public void setLinear(boolean isLinear) {
+		this.isLinear = isLinear;
+	}
+	public int getMoleculeID() {
+		return this.moleculeID;
+	}
+	 
 }
--- src/Domain/GameObjects/PowerUp.java
+++ src/Domain/GameObjects/PowerUp.java
@@ -1,5 +1,36 @@
 package Domain.GameObjects;
 
-public class PowerUp {
+import java.awt.Point;
 
+public class PowerUp {
+	public int powerUpID;
+	private Point coordinate;
+	private boolean isThrown;
+	
+	public PowerUp(int powerUpID, Point coordinate) {
+		this.powerUpID = powerUpID;
+		this.coordinate = coordinate;
+		this.isThrown = false;
+	} 
+	public PowerUp(int powerUpID, Point coordinate, boolean isThrown) {
+		this.powerUpID = powerUpID;
+		this.coordinate = coordinate;
+		this.isThrown = isThrown;
+	} 
+	public Point getCoordinate() {
+		return coordinate;
+	}
+	public void setCoordinate(Point coordinate) {
+		this.coordinate = coordinate;
+	}
+	public boolean isThrown() {
+		return isThrown;
+	}
+	public void setThrown(boolean isThrown) {
+		this.isThrown = isThrown;
+	}
+	public int getPowerUpID() {
+		return powerUpID;
+	}
+	
 }
