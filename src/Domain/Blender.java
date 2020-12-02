bdf4101b1b9f4a6927b8b0f10f790e605cdddaed
ygazi16 <ygazi16@ku.edu.tr>
2020-12-02 23:50:53 +0300

    blender 0.1

--- src/Domain/Blender.java
+++ src/Domain/Blender.java
@@ -1,5 +1,24 @@
 package Domain;
 
-public class Blender {
+import Domain.Player.Inventory;
 
-}
+public static class Blender {
+   
+  
+   protected static void useBlender(int sourceAtom, int goalAtom, Inventory inventory ) {
+      
+      if(verify(sourceAtom,goalAtom, inventory)) {
+      inventory.addInventoryAtom(goalAtom);
+      inventory.removeInventoryAtom(sourceAtom);
+      }
+    	
+   }
+   
+    protected static boolean verify(int SA,int GA, Inventory inventory){
+        /*while((!(SA==1) || !(SA==2) || SA==3 ||SA==4) || (!(GA==1) || !(GA==2) || !(GA==3) || !(GA==4))){
+            //User entered an invalid value, program asks user again and again until user enters the proper values.
+            useBlender(SA,GA, Inventory,inventory);
+        }*/
+    	//TODO add exception for invalid input/output combination
+    	return true;
+    } 
