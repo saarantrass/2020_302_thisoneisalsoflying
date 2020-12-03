8b8774b503333dcba91926ff0677a5d3f80a0d43
Ceyhun Aslan <ceyhunaslan18@gmail.com>
2020-12-03 16:19:53 +0300

    added settings and gameController

--- src/Domain/GameController.java
+++ src/Domain/GameController.java
@@ -1,5 +1,38 @@
 package Domain;
 
-public class GameController {
+import Domain.Player.Player;
 
+public class GameController {
+	public Player player;
+	
+	public void useBlender(int source, int goal) {
+		
+	}
+	public void editInBuildMode() {
+		
+	}
+	public int getRandomAtom() {
+		return 0;
+	}
+	public void moveShooter(int dir) {
+		
+	}
+	public void rotateShooter(int dir) {
+		
+	}
+	public void quitGame() {
+		
+	}
+	public void resumeGame() {
+		
+	}
+	public void pauseGame() {
+		
+	}
+	public void shootPowerUp(int powerUpID) {
+		
+	}
+	public void shootAtom(int atomID) {
+		
+	}
 }
