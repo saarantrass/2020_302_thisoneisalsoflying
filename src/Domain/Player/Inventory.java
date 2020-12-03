3cfcea005e1d5e9084565f3bca5d651e031e4ffc
unknown <bezirganmert@gmail.com>
2020-12-03 17:04:36 +0300

    inventory code added

--- src/Domain/Player/Inventory.java
+++ src/Domain/Player/Inventory.java
@@ -1,5 +1,55 @@
 package Domain.Player;
 
-public class Inventory {
+import java.util.HashMap;
 
+public class Inventory {
+	
+	private HashMap<Integer, Integer> inventoryAtom;
+	private HashMap<Integer, Integer> inventoryPowerUp;
+	
+	public Inventory(HashMap<Integer, Integer> inventoryAtom, HashMap<Integer, Integer> inventoryPoweUp) {
+		
+		this.inventoryAtom = new HashMap<Integer, Integer>();
+		this.inventoryPowerUp = new HashMap<Integer, Integer>();
+		
+		inventoryAtom.put(0, 0);
+		inventoryAtom.put(1, 0);
+		inventoryAtom.put(2, 0);
+		inventoryAtom.put(3, 0);
+		
+		inventoryPowerUp.put(0, 0);
+		inventoryPowerUp.put(1, 0);
+		inventoryPowerUp.put(2, 0);
+		inventoryPowerUp.put(3, 0);
+	}
+	
+	public void addInventoryAtom(int type) {
+		int curr = this.inventoryAtom.get(type);
+		this.inventoryAtom.replace(type, curr+1);
+	}
+	
+	public void addInventoryPowerUp(int type) {
+		int curr = this.inventoryPowerUp.get(type);
+		this.inventoryPowerUp.replace(type, curr+1);
+	}
+	
+	public void removeInventoryAtom(int type) {
+		int curr = this.inventoryAtom.get(type);
+		if(curr > 0) {
+			this.inventoryAtom.replace(type, curr-1);
+		}else {
+			//TODO add no more item case
+		}
+	}
+	
+	public void removeInventoryPowerUp(int type) {
+		int curr = this.inventoryPowerUp.get(type);
+		if(curr > 0) {
+			this.inventoryPowerUp.replace(type, curr-1);
+		}else {
+			//TODO add no more item case
+		}
+	}
+	
+	
 }
